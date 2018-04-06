afefa94a3d9a047f2cf8993bd7e5887b4f4fdbac
halukenes <halukenes97@gmail.com>
2018-04-06 14:46:37 +0300

    Updated and added comment to 'verification.js'

--- controllers/verification.js
+++ controllers/verification.js
@@ -3,6 +3,7 @@ module.exports = (req, res) => {
 
 const hubMode = req.query['hub.mode'];
  const verifyTokenMatches = (req.query['hub.verify_token'] === 'crowdbotics');
+ //'crowdbotics' can be changeable
 
 if (hubMode && verifyTokenMatches) {
  res.status(200).send(hubChallenge);
